8b49c24979c27cde07017d401bbd0e6784d86465
deebehygh <127646905+deebehygh@users.noreply.github.com>
2024-08-07 19:06:45 -0500

    Update Api.jsx

--- react-project/src/core/services/Api.jsx
+++ react-project/src/core/services/Api.jsx
@@ -1,17 +1,28 @@
 import axios from 'axios';
 
-const API_URL = 'https://0.0.0.0:3001/api/user';
+const API_URL = 'http://0.0.0.0:3001/api/user';
 
 export const signup = (email, username, password) => {
-  return axios.post(`${API_URL}/signup`, { email, username, password });
+  return axios.post(`${API_URL}/signup`, { email, username, password }, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    }
+  });
 };
 
 export const login = (username, password) => {
-  return axios.post(`${API_URL}/login`, { username, password });
+  return axios.post(`${API_URL}/login`, { username, password }, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    mode: 'no-cors',
+  });
 };
 
 export const createPost = (profile, content, token) => {
-  return axios.post(`https://0.0.0.0:3001/api/posts`, { profile, content }, {
+  return axios.post(`http://0.0.0.0:3001/api/posts`, { profile, content }, {
     headers: {
       Authorization: token
     }
@@ -19,7 +30,13 @@ export const createPost = (profile, content, token) => {
 };
 
 export const getPosts = () => {
-  return axios.get(`https://0.0.0.0:3001/api/posts`);
+  return axios.get(`http://0.0.0.0:3001/api/posts`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    mode: 'no-cors',
+  });
 };
 
 export const getProfile = (token) => {
